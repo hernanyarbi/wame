b4c470ceeb4dfc7959d122b6535d1b90cf6cb74d
Hernan Yarbi <hernan.yarbi@gmail.com>
2024-12-31 13:03:30 -0300

    feat: Add talwind and Hero Section with chip and title

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,12 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HeroSection from './components/HeroSection.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="relative min-h-screen w-full bg-neutral-900">
+    <div class="absolute inset-0 bg-green-900 bg-[size:20px_20px] opacity-20 blur-[100px]"></div>
+    <main class="text-gray-200 flex justify-center h-full">
+      <HeroSection />
+    </main>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ src/components/HeroSection.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="flex flex-col items-center justify-center h-full gap-10 py-20 max-w-4xl">
+    <div class="rounded-full py-1 px-4 bg-black border-2 border-sky-400">
+      Crea tu Enlace de WhatsApp en Segundos 📲✨
+    </div>
+    <h1 class="text-6xl text-center font-bold">
+      Conecta Fácilmente con tus Clientes,
+      <span class="bg-gradient-to-r from-green-400 to-sky-700 bg-clip-text text-transparent"
+        >¡Solo Ingresa un Número!</span
+      >
+    </h1>
+    <div>input</div>
+  </section>
+</template>
